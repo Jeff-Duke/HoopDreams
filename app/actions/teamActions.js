9c0c0f7e2165458bed80cc605c3a383432f71e24
David Kerr <kerrd89@gmail.com>
2016-12-18 16:21:13 -0700

    removing unused dependencies from team actions file

--- app/actions/teamActions.js
+++ app/actions/teamActions.js
@@ -1,16 +1,10 @@
 'use strict';
 import { types } from './actionTypes';
-import axios from 'axios';
-import path from 'path';
-// const teamInfo = require(path.join(__dirname, '/data/teams');
 
 export const actionCreators = {
   getTeam: (teamInfo) => {
-    //do stuff
     .then(data => {
       return { type: types.GET_TEAM, data: data }
-    })
-    console.log(teamData);
-    ;
+    });
   }
 };
